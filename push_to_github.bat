5d200c42b2224cfdc4ad3f55ba00161650e1bd29
BorStarcic <bs4586@student.uni-lj.si>
2026-02-09 17:38:51 +0100

    Update

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,7 +1,7 @@
 @echo off
 git init
 git add .
-git commit -m "Initial commit"
+git commit -m "Update"
 git branch -M main
 git remote add origin https://github.com/Bor-S/PingpongTracker.git
 git push -u origin main
